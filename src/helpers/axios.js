eb235df37cc790203ba2ec388c59fb31aefa3c05
Bartek0074 <bartekdo123@gmail.com>
2023-10-13 22:32:19 +0200

    added rapid key instead of env

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -4,7 +4,7 @@ export default axios.create({
 	baseURL: 'https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
 	headers: {
 		accept: 'application/json',
-		'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+		'X-RapidAPI-Key': '0305113434msh91a23d7be1ae531p1c4c59jsnff5ea436e476',
 		'X-RapidAPI-Host': 'matchilling-chuck-norris-jokes-v1.p.rapidapi.com',
 	},
 });
